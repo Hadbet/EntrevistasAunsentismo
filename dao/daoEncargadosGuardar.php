55945f69ba7f1e108bbe99d4afa56416aea5b912
hadbet <hadbet.altamirano@grammer.com>
2024-01-17 17:25:29 -0600

    Configuracion de dashboar por fechas

--- dao/daoEncargadosGuardar.php
+++ dao/daoEncargadosGuardar.php
@@ -1,10 +1,10 @@
 <?php
 include_once('db/db_RH.php');
 
-$Id=$_GET['id'];
-$APU=$_GET['apu'];
-$Supervisor=$_GET['supervisor'];
-$ShiftLeader=$_GET['shiftLeader'];
+$Id=$_POST['id'];
+$APU=$_POST['apu'];
+$Supervisor=$_POST['supervisor'];
+$ShiftLeader=$_POST['shiftLeader'];
 
 actualizarVacas($Id,$APU,$Supervisor,$ShiftLeader);
 
@@ -12,13 +12,19 @@ function actualizarVacas($Id,$APU,$Supervisor,$ShiftLeader) {
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    echo "UPDATE `Encargados` SET APU`='$APU',`Supervisor`='$Supervisor',`ShiftLeader`='$ShiftLeader' WHERE `IdEncargado` = '$Id'";
-    echo "INSERT INTO `Encargados`(`APU`, `Supervisor`, `ShiftLeader`) VALUES ('$APU','$Supervisor','$ShiftLeader')";
-
+    if ($Id != "n") {
+        $updateQuery = "UPDATE `Encargados` SET `APU`='$APU',`Supervisor`='$Supervisor',`ShiftLeader`='$ShiftLeader' WHERE `IdEncargado` = '$Id'";
+        $result = mysqli_query($conex, $updateQuery);
+        echo 2;
+    } else {
+        $insertQuery = "INSERT INTO `Encargados`(`APU`, `Supervisor`, `ShiftLeader`) VALUES ('$APU','$Supervisor','$ShiftLeader')";
+        $result = mysqli_query($conex, $insertQuery);
+        echo 1;
+    }
 
     mysqli_close($conex);
 
-    //return $result;
+    return $result;
 }
 
 ?>
