dca624cf97a9a0c02ed9ef49710ce71d9459e8f8
hadbet <hadbet.altamirano@grammer.com>
2024-01-17 17:21:54 -0600

    Configuracion de dashboar por fechas

--- dao/daoEncargadosGuardar.php
+++ dao/daoEncargadosGuardar.php
@@ -12,15 +12,9 @@ function actualizarVacas($Id,$APU,$Supervisor,$ShiftLeader) {
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    if ($Id != "n") {
-        $updateQuery = "UPDATE `Encargados` SET APU`='$APU',`Supervisor`='$Supervisor',`ShiftLeader`='$ShiftLeader' WHERE `IdEncargado` = '$Id'";
-        $result = mysqli_query($conex, $updateQuery);
-        echo 2;
-    } else {
-        $insertQuery = "INSERT INTO `Encargados`(`APU`, `Supervisor`, `ShiftLeader`) VALUES ('$APU','$Supervisor','$ShiftLeader')";
-        $result = mysqli_query($conex, $insertQuery);
-        echo 1;
-    }
+    echo "UPDATE `Encargados` SET APU`='$APU',`Supervisor`='$Supervisor',`ShiftLeader`='$ShiftLeader' WHERE `IdEncargado` = '$Id'";
+    echo "INSERT INTO `Encargados`(`APU`, `Supervisor`, `ShiftLeader`) VALUES ('$APU','$Supervisor','$ShiftLeader')";
+
 
     mysqli_close($conex);
 
