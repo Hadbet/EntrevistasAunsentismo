61a1ee217ced026cf26b22888b64d0a89a72c45a
hadbet <hadbet.altamirano@grammer.com>
2024-01-17 17:16:13 -0600

    Configuracion de dashboar por fechas

--- dao/daoEncargadosGuardar.php
+++ dao/daoEncargadosGuardar.php
@@ -1,10 +1,10 @@
 <?php
 include_once('db/db_RH.php');
 
-$Id=$_POST['id'];
-$APU=$_POST['apu'];
-$Supervisor=$_POST['supervisor'];
-$ShiftLeader=$_POST['shiftLeader'];
+$Id=$_GET['id'];
+$APU=$_GET['apu'];
+$Supervisor=$_GET['supervisor'];
+$ShiftLeader=$_GET['shiftLeader'];
 
 actualizarVacas($Id,$APU,$Supervisor,$ShiftLeader);
 
