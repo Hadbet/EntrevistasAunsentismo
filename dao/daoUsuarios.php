8001bfcde261ed3faea110f0c51cffa7e888ab5b
hadbet <hadbet.altamirano@grammer.com>
2024-01-17 17:51:42 -0600

    Configuracion de dashboar por fechas

--- dao/daoUsuarios.php
+++ dao/daoUsuarios.php
@@ -9,7 +9,7 @@ function Contador()
     $con = new LocalConector();
     $conex = $con->conectar();
 
-    $datos = mysqli_query($conex, "SELECT `IdNomina`,`Nombre`,`APU`,`Puesto`,`Rol`,`Password`,CONCAT('<button class=\"primary\" onclick=\"llenarParametros(','\'',`IdNomina`,'\',\'',`Nombre`,'\',\'',`APU`,'\',\'',`Puesto`,'\',\'',`Rol`,'\',\'',`Password`,'\');\" style=\"background-color: #547fea\">Editar-Eliminar</button>') as boton FROM `UsuariosEntrevistas`");
+    $datos = mysqli_query($conex, "SELECT `IdNomina`,`Nombre`,`APU`,`Puesto`,`Rol`,`Password`,CONCAT('<button class=\"primary\" onclick=\"llenarParametrosUsu(','\'',`IdNomina`,'\',\'',`Nombre`,'\',\'',`APU`,'\',\'',`Puesto`,'\',\'',`Rol`,'\',\'',`Password`,'\');\" style=\"background-color: #547fea\">Editar-Eliminar</button>') as boton FROM `UsuariosEntrevistas`");
 
     $resultado = mysqli_fetch_all($datos, MYSQLI_ASSOC);
     echo json_encode(array("data" => $resultado));
