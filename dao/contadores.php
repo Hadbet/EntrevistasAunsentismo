d0a466a57c79fd2071ed60455dc7338caff87ffa
hadbet <hadbet.altamirano@grammer.com>
2024-01-17 16:41:22 -0600

    Configuracion de dashboar por fechas

--- dao/contadores.php
+++ dao/contadores.php
@@ -21,28 +21,28 @@ function Contador($Ruta,$APU,$FechaInicio,$FechaFin)
     }
 
     if ($Ruta == 2){
-        $query = "SELECT COUNT(`IdEntrevista`) as contador, `ShiftLeader` FROM `EntrevistasAusentismo` where `Encargado` like '%$APU%' and `FechaAusentismo` BETWEEN '$FechaInicio' and '$FechaFin' GROUP BY `ShiftLeader`;";
+        $query = "SELECT COUNT(`IdEntrevista`) as contador, `ShiftLeader` FROM `EntrevistasAusentismo` where `Encargado` like '%$APU%' and `FechaAusentismo` BETWEEN '$FechaInicio' and '$FechaFin' GROUP BY `ShiftLeader` ORDER by contador desc;";
     }
 
     if ($Ruta == 3){
-        $query = "SELECT COUNT(`IdEntrevista`) as contador, `TipoAusencia` FROM `EntrevistasAusentismo` where `Encargado` like '%$APU%'  and `FechaAusentismo` BETWEEN '$FechaInicio' and '$FechaFin' GROUP BY `TipoAusencia`;";
+        $query = "SELECT COUNT(`IdEntrevista`) as contador, `TipoAusencia` FROM `EntrevistasAusentismo` where `Encargado` like '%$APU%'  and `FechaAusentismo` BETWEEN '$FechaInicio' and '$FechaFin' GROUP BY `TipoAusencia` ORDER by contador desc;";
     }
 
     if ($Ruta == 4){
-        $query = "SELECT COUNT(`IdEntrevista`) as contador, `Area` FROM `EntrevistasAusentismo` where `Encargado` like '%$APU%'  and `FechaAusentismo` BETWEEN '$FechaInicio' and '$FechaFin' GROUP BY `Area`;";
+        $query = "SELECT COUNT(`IdEntrevista`) as contador, `Area` FROM `EntrevistasAusentismo` where `Encargado` like '%$APU%'  and `FechaAusentismo` BETWEEN '$FechaInicio' and '$FechaFin' GROUP BY `Area` ORDER by contador desc;";
     }
 
     if ($Ruta == 5){
-        $query = "SELECT COUNT(`IdEntrevista`) as contador, `NominaEntrevistado` FROM `EntrevistasAusentismo` where `Encargado` like '%$APU%' and `FechaAusentismo` BETWEEN '$FechaInicio' and '$FechaFin' GROUP BY `NominaEntrevistado`;";
+        $query = "SELECT COUNT(`IdEntrevista`) as contador, `NominaEntrevistado` FROM `EntrevistasAusentismo` where `Encargado` like '%$APU%' and `FechaAusentismo` BETWEEN '$FechaInicio' and '$FechaFin' GROUP BY `NominaEntrevistado` ORDER by contador desc;";
     }
 
     if ($Ruta == 6){
         $APU = str_replace("APU ", "", $APU);
-        $query = "SELECT COUNT(`IdEntrevista`) as contador,`FechaAusentismo` FROM `EntrevistasAusentismo` WHERE `NominaEntrevistado` = '$APU'  GROUP BY `FechaAusentismo`;";
+        $query = "SELECT COUNT(`IdEntrevista`) as contador,`FechaAusentismo` FROM `EntrevistasAusentismo` WHERE `NominaEntrevistado` = '$APU'  GROUP BY `FechaAusentismo` ORDER by contador desc;";
     }
 
     if ($Ruta == 7){
-        $query = "SELECT COUNT(`IdEntrevista`) as contador,`Supervisor` FROM `EntrevistasAusentismo` where `Encargado` = '$APU' and `FechaAusentismo` BETWEEN '$FechaInicio' and '$FechaFin' GROUP BY `Supervisor`;";
+        $query = "SELECT COUNT(`IdEntrevista`) as contador,`Supervisor` FROM `EntrevistasAusentismo` where `Encargado` = '$APU' and `FechaAusentismo` BETWEEN '$FechaInicio' and '$FechaFin' GROUP BY `Supervisor` ORDER by contador desc;";
     }
 
     $datos = mysqli_query($conex, $query);
