6478a11f57b6849e60e8da3ff70906872af4f8a3
hadbet <hadbet.altamirano@grammer.com>
2024-01-29 15:20:03 -0600

    Limpieza de codogio

--- Administracion.php
+++ Administracion.php
@@ -15,6 +15,7 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no"/>
     <link rel="stylesheet" href="assets/css/main.css"/>
+    <link rel="stylesheet" href="lib/css/noicartsinimda.css"/>
 
     <!-- CSS -->
     <link rel="stylesheet" href="lib/sweetalert2.css">
