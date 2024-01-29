ed4d07e71a87f5b8dc75ffe12f776ae391d003a3
hadbet <hadbet.altamirano@grammer.com>
2024-01-29 15:10:20 -0600

    Cambio de color y se agrego "Salir"

--- Historico.php
+++ Historico.php
@@ -207,43 +207,11 @@
 <script type="text/javascript" charset="utf8"
         src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.js"></script>
+<script src="lib/js/niam.js"></script>
+<script src="lib/js/socirotsih.js"></script>
 
 <script>
 
-    var rol = '<?php echo $_SESSION["rol"];?>';
-
-    if (rol==='2'){
-        window.location.href = 'index.php';
-    }
-    function inicializarTabla() {
-        $('#miTabla').DataTable({
-            ajax: {
-                url: 'https://arketipo.mx/RH/Entrevistas/dao/daoHistoricoGeneral.php',
-                dataSrc: 'data'
-            },
-            columns: [
-                {data: 'NominaEntrevistado'},
-                {data: 'FechaAusentismo'},
-                {data: 'TipoAusencia'},
-                {data: 'Motivo'},
-                {data: 'Area'},
-                {data: 'Encargado'},
-                {data: 'Supervisor'},
-                {data: 'ShiftLeader'},
-                {data: 'Tag'}
-            ]
-        });
-    }
-
-    // Llama a la función al cargar la página
-    $(document).ready(function () {
-        inicializarTabla();
-    });
-
-    function toggleMenu() {
-        var menu = document.getElementById("menuNavegacion");
-        menu.style.display = (menu.style.display === "block") ? "none" : "block";
-    }
 </script>
 
 </body>
